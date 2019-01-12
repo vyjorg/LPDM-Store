bd66cb0ea7e0c11a8c958fe9a7af1f3ad6c0d8ce
Kybox <nslr@riseup.net>
2019-01-12 16:20:30 +0100

    Adding Zuul urls & Eureka dependency

--- src/main/java/com/lpdm/msstore/proxy/LocationProxy.java
+++ src/main/java/com/lpdm/msstore/proxy/LocationProxy.java
@@ -1,6 +1,7 @@
 package com.lpdm.msstore.proxy;
 
 import com.lpdm.msstore.model.Location;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
-@FeignClient(name = "ms-location",
-        url = "${microservice.domain:https://location.lpdm.kybox.fr}")
+@FeignClient(name = "${lpdm.zuul.name}", url = "${lpdm.zuul.uri}")
+@RibbonClient("${lpdm.location.name}")
 public interface LocationProxy {
 
     @RequestMapping(value = "/locations/{id}",
--- src/main/java/com/lpdm/msstore/proxy/OrderProxy.java
+++ src/main/java/com/lpdm/msstore/proxy/OrderProxy.java
@@ -1,6 +1,7 @@
 package com.lpdm.msstore.proxy;
 
 import com.lpdm.msstore.model.Order;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
-@FeignClient(name = "ms-order",
-        url = "${microservice.domain:https://order.lpdm.kybox.fr}")
+@FeignClient(name = "${lpdm.zuul.name}", url = "${lpdm.zuul.uri}")
+@RibbonClient("${lpdm.order.name}")
 public interface OrderProxy {
 
     @RequestMapping(value = "orders/{id}",
